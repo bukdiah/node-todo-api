314e858d55dafb0e4f7b2b8f7d6e8496e7a80336
kevinsounthavong <soun042564@gmail.com>
2018-03-21 13:39:02 -0400

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -32,6 +32,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+// GET to RETRIEVE todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos: todos
+    })
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
