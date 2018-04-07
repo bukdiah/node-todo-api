fbdca4ef253de423e4a5a690f724d2a9f3e9a8ea
kevinsounthavong <soun042564@gmail.com>
2018-04-07 13:14:37 -0400

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -326,3 +326,24 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    console.log("DELETE user token value ", users[0].tokens[0].token)
+    request(app)
+      .delete('/users/me/token')
+      .set("x-auth", users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
